9508a9a2eba674bd59531bc2a27807c977b740f4
muhersi30 <Mustafa.hersi6@gmail.com>
2022-09-15 00:35:36 +0500

    Assignment2

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Setting name of app in title bar
         getSupportActionBar()?.setTitle(R.string.app_name);
     }
 }
